c5cec5182bb4afe66f78b9efb55cce07ccd60ef9
yr <yr186@qq.com>
2023-05-15 01:58:04 +0800

    :thumbsup:

--- action-build.sh
+++ action-build.sh
@@ -4,13 +4,13 @@ set -e
 
 target="${1:-release}"
 
-# export BUILD_UNIVERSAL=1
+export BUILD_UNIVERSAL=1
 
 # preinstall
 ./action-install.sh
 
 # build dependencies
-# make deps
+make deps
 
 # build Squirrel
 make MACOSX_DEPLOYMENT_TARGET='13.0' "${target}"
